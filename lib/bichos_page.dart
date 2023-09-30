967d431767b5faad88a18a9c963140513ef81e37
Andre Lucas <alucas748@gmail.com>
2023-09-30 14:31:18 -0300

    Editando a interface com GridView e MediaQuery

--- lib/bichos_page.dart
+++ lib/bichos_page.dart
@@ -10,6 +10,39 @@ class BichosPagina extends StatefulWidget {
 class _BichosPaginaState extends State<BichosPagina> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+
+    //double largura = MediaQuery.of(context).size.width;
+    //double altura = MediaQuery.of(context).size.height;
+    
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset("assets/images/vaca.png"),
+        )
+      ],
+    );
   }
 }
