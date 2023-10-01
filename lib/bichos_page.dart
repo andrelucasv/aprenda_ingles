9a05f842ead331adaf791625d9891c6b69bc68a9
Andre Lucas <alucas748@gmail.com>
2023-10-01 02:15:16 -0300

    Finalizado

--- lib/bichos_page.dart
+++ lib/bichos_page.dart
@@ -1,5 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class BichosPagina extends StatefulWidget {
   const BichosPagina({super.key});
--- lib/numeros_page.dart
+++ lib/numeros_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class NumerosPagina extends StatefulWidget {
   const NumerosPagina({super.key});
@@ -7,9 +8,60 @@ class NumerosPagina extends StatefulWidget {
   State<NumerosPagina> createState() => _NumerosPaginaState();
 }
 
+final audioPlayer = AudioPlayer();
+
+  _executarAudio(String nomeAudio) async {
+
+    await audioPlayer.play(AssetSource("audios/$nomeAudio.mp3"));
+
+  }
+
 class _NumerosPaginaState extends State<NumerosPagina> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: () {
+            _executarAudio("1");
+          },
+          child: Image.asset("assets/images/1.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executarAudio("2");
+          },
+          child: Image.asset("assets/images/2.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executarAudio("3");
+          },
+          child: Image.asset("assets/images/3.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executarAudio("4");
+          },
+          child: Image.asset("assets/images/4.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executarAudio("5");
+          },
+          child: Image.asset("assets/images/5.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executarAudio("6");
+          },
+          child: Image.asset("assets/images/6.png"),
+        )
+      ],
+    );
+
   }
 }
